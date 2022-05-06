5a13e25b975bb9720727ca17bbcf2d087602892e
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-05-06 09:31:50 -0500

    Previo a prueba

--- JavaScript/JSON-API-2.js
+++ JavaScript/JSON-API-2.js
@@ -10,17 +10,17 @@ fetch(url).then(function(respuesta)
 {
     return respuesta.json();
 }
-).then(function(Diputado1){
+).then(function(DiputadosMorena){
 
     index = 1;
     UbicaciónInserción.innerHTML = "Soy Dante";
-    UbicaciónInserción.innerHTML =  "<p>"+Diputado1[index]["número"]+" "+Diputada1[index]["nombre"]+" "+Diputada1[index]["apellidoPaterno"]+" "+Diputada1[index]["apellidoMaterno"]+"del estado de "+Diputada1[index]["entidad"]+"de "+Diputada1[index]["distrito_circunscripcion"]+" de la bancada de "+Diputada1[index]["partido"]+"</p>";
+    UbicaciónInserción.innerHTML =  "<p>"+DiputadosMorena[index]["número"]+" "+DiputadosMorena[index]["nombre"]+" "+DiputadosMorena[index]["apellidoPaterno"]+" "+DiputadosMorena[index]["apellidoMaterno"]+"del estado de "+DiputadosMorena[index]["entidad"]+"de "+DiputadosMorena[index]["distrito_circunscripcion"]+" de la bancada de "+DiputadosMorena[index]["partido"]+"</p>";
 
 
-    console.log(Diputada1);
-    for(let i = 0; i < Diputada1.length; i++)
+    console.log(DiputadosMorena);
+    for(let i = 0; i < DiputadosMorena.length; i++)
     {
-        UbicaciónInserción.innerHTML +=  "<div class='poster'><img src='"+uri+Diputada1[i]["imagen"]+"'><h3>"+Diputada1[i]["nombre"]+"</h3><div></div>";
+        UbicaciónInserción.innerHTML +=  "<div class='poster'><img src='"+uri+DiputadosMorena[i]["imagen"]+"'><h3>"+DiputadosMorena[i]["nombre"]+"</h3><div></div>";
     }
 }
 ).catch(function(error)
