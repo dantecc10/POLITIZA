e233fdab6052239715069ab61f00d2944fec10c0
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-05-06 23:43:13 -0500

    Dante

--- JavaScript/API-JSON-Diputados.js
+++ JavaScript/API-JSON-Diputados.js
@@ -1,1 +1,71 @@
-const contenedor = documento.getElementByID("contenido-principal");const url_DMorena = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosMorena.json" //"https://https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosMorena.json";const url_DPAN = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPAN.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPAN.json";const url_DPRI = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPRI.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPRI.json";const url_DPVEM = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPVEM.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPVEM.json";const url_DPT = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPT.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPT.json";const url_DMC = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosMC.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosMC.json";const url_DPRD = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPRD.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPRD.json";const url_DApartidistas = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosApartidistas.json";const url_general = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/";{ "employees":[        {"firstName": "John", "lastName": "Doe"},        {"firstName": "Anna", "lastName": "Smith"},        {"firstName": "Peter", "lastName": "Jones"}    ]}let text = '{ "employees" : [' + '{"firstName": "John" , "lastName": "Doe" },' + '{"firstName": "Anna" , "lastName": "Smith" },' + '{"firstName": "Peter" , "lastName": "Jones" }';const obj = JSON.parse()/*fetch(url_DMorena).then(function(respuestaMorena){    return respuestaMorena.json();}).then(function(datosMorena){{    let i = 1;    json [let] ["nombre"];    document.getElementById("pPrueba").innerText=("El (la) primer@ diputad@ de Morena es: " + respuestaMorena);    /*    //Modificar el HTML    for (let i = 1; i < json.length; i++)    {        json[i]["número"]        for(let j = 1; j < json [i] ["albums"].length; j++)        {            json [i] ["número"] [j] ["nombre"]            for(let k = 1; k < json[1]  )            json [i] ["albums"] [j] ["imagen"]        }    }    json[0]["albums"][1]["imagen"]    datos[1][""]}*//*    console.log(datos);    for (let i = 0; i < datos.length; i++)    {        contenedor.innerHTML += "<div class='poster'><img src'"+url_general+datos[i]["imagen"]+"'><h3>"+datos[i]["nombre"]+"</h3></div>";    }}).catch(function(error)    {        console.log(error);    });*/
+const contenedor = documento.getElementByID("contenido-principal");
+
+const url_DMorena = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosMorena.json" //"https://https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosMorena.json";
+const url_DPAN = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPAN.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPAN.json";
+const url_DPRI = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPRI.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPRI.json";
+const url_DPVEM = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPVEM.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPVEM.json";
+const url_DPT = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPT.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPT.json";
+const url_DMC = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosMC.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosMC.json";
+const url_DPRD = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA/DiputadosPRD.json" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosPRD.json";
+const url_DApartidistas = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosApartidistas.json";
+
+const url_general = "D:/MEGA/MEGA CLOUD/DANTE/ACADÉMICO/API POLITIZA" //"https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/";
+
+{ "employees":[
+        {"firstName": "John", "lastName": "Doe"},
+        {"firstName": "Anna", "lastName": "Smith"},
+        {"firstName": "Peter", "lastName": "Jones"}
+    ]}
+
+let text = '{ "employees" : [' + '{"firstName": "John" , "lastName": "Doe" },' + '{"firstName": "Anna" , "lastName": "Smith" },' + '{"firstName": "Peter" , "lastName": "Jones" }';
+
+const obj = JSON.parse()
+
+
+
+
+
+
+
+
+
+/*
+fetch(url_DMorena).then(function(respuestaMorena)
+{
+    return respuestaMorena.json();
+}).then(function(datosMorena){
+{
+    let i = 1;
+    json [let] ["nombre"];
+    document.getElementById("pPrueba").innerText=("El (la) primer@ diputad@ de Morena es: " + respuestaMorena);
+
+
+
+    /*
+    //Modificar el HTML
+    for (let i = 1; i < json.length; i++)
+    {
+        json[i]["número"]
+
+        for(let j = 1; j < json [i] ["albums"].length; j++)
+        {
+            json [i] ["número"] [j] ["nombre"]
+            for(let k = 1; k < json[1]  )
+            json [i] ["albums"] [j] ["imagen"]
+        }
+    }
+    json[0]["albums"][1]["imagen"]
+    datos[1][""]}*/
+
+
+/*
+    console.log(datos);
+    for (let i = 0; i < datos.length; i++)
+    {
+        contenedor.innerHTML += "<div class='poster'><img src'"+url_general+datos[i]["imagen"]+"'><h3>"+datos[i]["nombre"]+"</h3></div>";
+    }
+}).catch(function(error)
+    {
+        console.log(error);
+    });
+*/
--- JavaScript/Redes Sociales.js
+++ JavaScript/Redes Sociales.js
@@ -1,1 +1,29 @@
-//Declaración de funciones de redes socialesfunction AbrirFacebook() {    alert("Le redirigiré a mi perfil de Facebook...");    window.open('https://www.facebook.com/DanteCC10.4T', '_blank');}function AbrirWhatsApp() {    alert("Le redirigiré a un chat conmigo en WhatsApp...");    window.open('https://wa.me/527979773095', '_blank');}function AbrirMessenger() {    alert("Le redirigiré a un chat conmigo en Messenger...");    window.open('https://m.me/dantecc10fi', '_blank');}function AbrirYouTube() {    alert("Le redirigiré mi canal de YouTube...");    window.open('https://www.youtube.com/channel/UCXjG-Lpe_lEUwVdBHS2u3Aw', '_blank');}function AbrirTelegram() {    alert("Le redirigiré a un chat conmigo en Telegram...");    window.open('https://t.me/dantecc10', '_blank');}function AbrirTwitter() {    alert("Le redirigiré a mi cuenta de Twitter...");    window.open('https://www.twitter.com/dantecc10', '_blank');}function AbrirGitHub() {    alert("Le redirigiré a mis respositorios de GitHub...");    window.open('https://www.github.com/dantecc10', '_blank');}
+//Declaración de funciones de redes sociales
+function AbrirFacebook() {
+    alert("Le redirigiré a mi perfil de Facebook...");
+    window.open('https://www.facebook.com/DanteCC10.4T', '_blank');
+}
+function AbrirWhatsApp() {
+    alert("Le redirigiré a un chat conmigo en WhatsApp...");
+    window.open('https://wa.me/527979773095', '_blank');
+}
+function AbrirMessenger() {
+    alert("Le redirigiré a un chat conmigo en Messenger...");
+    window.open('https://m.me/dantecc10fi', '_blank');
+}
+function AbrirYouTube() {
+    alert("Le redirigiré mi canal de YouTube...");
+    window.open('https://www.youtube.com/channel/UCXjG-Lpe_lEUwVdBHS2u3Aw', '_blank');
+}
+function AbrirTelegram() {
+    alert("Le redirigiré a un chat conmigo en Telegram...");
+    window.open('https://t.me/dantecc10', '_blank');
+}
+function AbrirTwitter() {
+    alert("Le redirigiré a mi cuenta de Twitter...");
+    window.open('https://www.twitter.com/dantecc10', '_blank');
+}
+function AbrirGitHub() {
+    alert("Le redirigiré a mis respositorios de GitHub...");
+    window.open('https://www.github.com/dantecc10', '_blank');
+}
