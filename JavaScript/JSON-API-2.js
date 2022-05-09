0301f640352bd2c13cedeeff66d48caca687d6a3
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-05-08 22:54:07 -0500

    const dttos, añadido filtro Estados, Distritos (p)

--- JavaScript/JSON-API-2.js
+++ JavaScript/JSON-API-2.js
@@ -221,7 +221,7 @@ function leerPartido() {
 
     document.getElementById("Experimental").innerHTML = (nuevaVar);
 
-    switch (document.getElementsByTagName("select").Partido.selectedOptions[0].outerText) { /*¡Éxito!*/
+    switch (stringSeleccionadoPartido) { /*¡Éxito!*/
         case "Morena":
             //búsquedaMorena();
             urlVariable = "https://raw.githubusercontent.com/dantecc10/API-POLITIZA/master/DiputadosMorena.json";
@@ -264,8 +264,141 @@ function leerPartido() {
             break;
         default:
             document.getElementById("InsertarResultados").innerHTML = ("");
-            if((document.getElementsByTagName("select").Partido.selectedOptions[0].outerText)!="Sin partido")
-            console.log("Ya casi, el problema es al comparar...");
+            if ((document.getElementsByTagName("select").Partido.selectedOptions[0].outerText) != "Sin partido")
+                console.log("Ya casi, el problema es al comparar...");
+    }
+
+    switch (stringSeleccionadoEstado) {
+        case "Aguascalientes":
+            console.log("Usted seleccionó el estado de Aguascalientes");
+            break;
+        case "Baja California":
+            console.log("Usted seleccionó el estado de Baja California");
+            break;
+        case "Baja California Sur":
+            console.log("Usted seleccionó el estado de Baja California Sur");
+            break;
+        case "Campeche":
+            console.log("Usted seleccionó el estado de Campeche");
+            break;
+        case "Chiapas":
+            console.log("Usted seleccionó el estado de Chiapas");
+            break;
+        case "Chihuahua":
+            console.log("Usted seleccionó el estado de Chihuahua");
+            break;
+        case "Ciudad de México":
+            console.log("Usted seleccionó el estado de Ciudad de México");
+            break;
+        case "Coahuila":
+            console.log("Usted seleccionó el estado de Coahuila");
+            break;
+        case "Colima":
+            console.log("Usted seleccionó el estado de Colima");
+            break;
+        case "Durango":
+            console.log("Usted seleccionó el estado de Durango");
+            break;
+        case "Estado de México":
+            console.log("Usted seleccionó el estado de Estado de México");
+            break;
+        case "Guanajuato":
+            console.log("Usted seleccionó el estado de Guanajuato");
+            break;
+        case "Guerrero":
+            console.log("Usted seleccionó el estado de Guerrero");
+            break;
+        case "Hidalgo":
+            console.log("Usted seleccionó el estado de Hidalgo");
+            break;
+        case "Jalisco":
+            console.log("Usted seleccionó el estado de Jalisco");
+            break;
+        case "Michoacán":
+            console.log("Usted seleccionó el estado de Michoacán");
+            break;
+        case "Morelos":
+            console.log("Usted seleccionó el estado de Morelos");
+            break;
+        case "Nayarit":
+            console.log("Usted seleccionó el estado de Nayarit");
+            break;
+        case "Nuevo León":
+            console.log("Usted seleccionó el estado de Nuevo León");
+            break;
+        case "Oaxaca":
+            console.log("Usted seleccionó el estado de Oaxaca");
+            break;
+        case "Puebla":
+            console.log("Usted seleccionó el estado de Puebla");
+            break;
+        case "Querétaro":
+            console.log("Usted seleccionó el estado de Querétaro");
+            break;
+        case "Quintana Roo":
+            console.log("Usted seleccionó el estado de Quintana Roo");
+            break;
+        case "San Luis Potosí":
+            console.log("Usted seleccionó el estado de San Luis Potosí");
+            break;
+        case "Sinaloa":
+            console.log("Usted seleccionó el estado de Sinaloa");
+            break;
+        case "Sonora":
+            console.log("Usted seleccionó el estado de Sonora");
+            break;
+        case "Tabasco":
+            console.log("Usted seleccionó el estado de Tabasco");
+            break;
+        case "Tamaulipas":
+            console.log("Usted seleccionó el estado de Tamaulipas");
+            break;
+        case "Tlaxcala":
+            console.log("Usted seleccionó el estado de Tlaxcala");
+            break;
+        case "Veracruz":
+            console.log("Usted seleccionó el estado de Veracruz");
+            break;
+        case "Yucatán":
+            console.log("Usted seleccionó el estado de Yucatán");
+            break;
+        case "Zacatecas":
+            console.log("Usted seleccionó el estado de Zacatecas");
+            break;
+
+    }
+}
+
+function comprobarFiltros() {
+    var usarFiltroPartidos = document.getElementById("FiltroPartido");
+    var usarFiltroEstados = document.getElementById("FiltroEstado");
+    var usarFiltroDttosCircs = document.getElementById("FiltroDttoCirc");
+
+    // Línea temporal de depuración: console.log(usarFiltroEstados)
+    //Comprobación de verificación de casilla para filtro de partidos
+    if (document.getElementById("FiltroPartido").checked == true) {
+        console.log("El filtro de partidos fue activado.");
+    }
+    else {
+        console.log("Hubo un error al leer la casilla de partidos, o está desactivado.");
+    }
+
+    if (document.getElementById("FiltroEstado").checked == true) {
+        const dttosAguascalientes = 3, dttosBajaCalifornia = 8, dttosBajaCaliforniaSur = 2, dttosCampeche = 2, dttosCDMX = 24, dttosChiapas = 13, dttosChihuahua = 9, dttosCoahuila = 7, dttosColima = 2, dttosDurango = 4, dttosEdoMex = 41, dttosGuanajuato = 15, dttosGuerrero = 9, dttosHidalgo = 7, dttosJalisco = 20, dttosMichoacán = 12, dttosMorelos = 5, dttosNayarit = 3, dttosNuevoLeón = 12, dttosOaxaca = 10, dttosPuebla = 15, dttosQuerétaro = 5, dttosQuintanaRoo = 4, dttosSanLuisPotosí = 7, dttosSinaloa = 7, dttosSonora = 7, dttosTabasco = 6, dttosTamaulipas = 9, dttosTlaxcala = 3, dttosVeracruz = 20, dttosYucatán = 5, dttosZacatecas = 4;
+
+        console.log("El filtro de estados fue activado.");
+        document.getElementById("Lista3").innerHTML += ("");
+    }
+    else {
+        console.log("Hubo un error al leer la casilla de estados, o está desactivado.");
+        document.getElementById("Lista3").innerHTML = ("");
+    }
+
+    if (document.getElementById("FiltroDttoCirc").checked == true) {
+        console.log("El filtro de distritos/circunscripciones fue activado.");
+    }
+    else {
+        console.log("Hubo un error al leer la casilla de distritos/circunscripciones, o está desactivado.");
     }
 }
 
