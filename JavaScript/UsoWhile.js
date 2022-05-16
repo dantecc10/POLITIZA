ea2d73f4407987778a5b9ded45d4648d4cf748d7
Dante Castelán Carpinteyro <dantecc10@gmail.com>
2022-05-16 00:30:40 -0500

    Nuevo script

---
+++ JavaScript/UsoWhile.js
@@ -0,0 +1,4 @@
+var finDelMundo = false;
+while (finDelMundo = false) {
+    document.getElementById("copyright").innerHTML = ("<b>Copyright © Dante Castelán Carpinteyro - <i>Führer Industries</i> - 2022</b>");
+}
